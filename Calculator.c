e31bb59f9cd120f5c913aa80c08c1ee621298837
sahiltejwani <tejwanisahil73@gmail.com>
2025-04-15 14:13:17 +0530

    First calculator code is added

---
+++ Calculator.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <math.h>
+
+int main() {
+    double num1, num2;
+    char operator;
+
+    printf("Simple Calculator in C\n");
+    printf("Enter an expression (e.g., 5 + 3): ");
+    scanf("%lf %c %lf", &num1, &operator, &num2);
+
+    switch (operator) {
+        case '+':
+            printf("%.2lf + %.2lf = %.2lf\n", num1, num2, num1 + num2);
+            break;
+        case '-':
+            printf("%.2lf - %.2lf = %.2lf\n", num1, num2, num1 - num2);
+            break;
+        case '*':
+            printf("%.2lf * %.2lf = %.2lf\n", num1, num2, num1 * num2);
+            break;
+        case '/':
+            if (num2 != 0)
+                printf("%.2lf / %.2lf = %.2lf\n", num1, num2, num1 / num2);
+            else
+                printf("Error: Division by zero!\n");
+            break;
+        default:
+            printf("Invalid operator\n");
+    }
+
+    return 0;
+}
